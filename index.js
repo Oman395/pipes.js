e3ebaae07aad71c56285688edd86f3ca784fe898
Oran RH <oranroha@gmail.com>
2023-03-07 11:13:56 -0500

    Added launch options, added help page, added configuration file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/bin/node
 
+const fs = require("fs");
+
 const CHARS = {
   ul: "╭",
   ur: "╮",
@@ -9,25 +11,107 @@ const CHARS = {
   ve: "│",
 };
 
+// Get command line arguments. There's libraries to do this, but I want this to work as a standalone program, so I just wrote
+// this scuffed version.
+
+let args = process.argv.slice(2, process.argv.length);
+let cfgPath = "~/.config/pipes.js/config.json";
+for (let i = 0; i < args.length; i++) {
+  let arg = args[i];
+  if (arg[0] === "-") {
+    switch (arg.slice(1, arg.length)) {
+      case "-help":
+        console.log(
+          `pipes.js help
+==> Arguments
+  --config,-c: Specify configuration path
+
+  --help,-h: Display this page
+
+==> Configuration
+  minimumDistanceBeforeTurn: Number of characters before being allowed to turn.
+
+  randomThreshold: Threshold for Math.random() to be under. Will increase exponentially if random mode is exponential.
+
+  randomMode: 0 = Exponential, RAND_AMOUNT ** x, 1 = Static.
+
+  unstretchVertical: Apply fix for vertical lines being taller than horizontal lines are long.
+
+  unstretchFactor: Factor to increase probability that the vertical line goes horizontal to. Ignored if unstretchVertical is true.
+
+  maximumCharacters: Maximum characters displayed before clearing screen. Infinity if -1.
+
+  numberOfHeads: Number of distinct pipes to draw at once.`
+        );
+        process.exit(1);
+      case "-config":
+        cfgPath = args[i + 1];
+        i++;
+        continue;
+      case "h":
+        process.exit(0);
+      case "c":
+        cfgPath = args[i + 1];
+        i++;
+        continue;
+      default:
+        console.error(
+          "Unknown argument (type -h for help)! Attempted to read: " + arg
+        );
+        process.exit(1);
+    }
+  } else {
+    console.error(
+      "Unknown argument (type -h for help)! Attempted to read: " + arg
+    );
+    process.exit(1);
+  }
+}
+
+// Default values
+
 // Number of characters before being allowed to turn
-const MIN_DIST_TURN = 1;
+let MIN_DIST_TURN = 1;
 // Threshold for Math.random() to be under, if the random mode is exponential then this will increase exponentially
-const RAND_AMOUNT = 0.01;
+let RAND_AMOUNT = 0.01;
 // 0 = Exponential, RAND_AMOUNT ** x
 // 1 = Static
-const RAND_MODE = 0;
+let RAND_MODE = 0;
 // Take a wild guess
-const FPS = 75;
+let FPS = 75;
 // Terminal colors or RGB in the format "r;g;b"
-const COLORS = [0, 1, 2, 3, 4, 5];
+let COLORS = [0, 1, 2, 3, 4, 5];
 // If true, when moving vertically, the random chance will be doubled
-const UNSTRETCH_VERTICAL = true;
+let UNSTRETCH_VERTICAL = true;
 // What to multiply the random amount by when we are moving vertically if UNSTRETCH_VERTICAL is true
-const UNSTRETCH_FACTOR = 5;
+let UNSTRETCH_FACTOR = 5;
 // Number of characters allowed to print on the screen; once this is passed, screen will clear
-const MAX_CHARS = Infinity;
+let MAX_CHARS = 20000;
 // Number of paths to run concurrently
-const PATH_COUNT = 1;
+let PATH_COUNT = 1;
+let delay = false;
+if (fs.existsSync(cfgPath)) {
+  try {
+    let opts = JSON.parse(fs.readFileSync(cfgPath).toString());
+    MIN_DIST_TURN = opts.minimumDistanceBeforeTurn ?? MIN_DIST_TURN;
+    RAND_AMOUNT = opts.randomThreshold ?? RAND_AMOUNT;
+    RAND_MODE = opts.randomMode ?? RAND_MODE;
+    FPS = opts.FPS ?? FPS;
+    COLORS = opts.colors ?? COLORS;
+    UNSTRETCH_VERTICAL = opts.unstretchVertical ?? UNSTRETCH_VERTICAL;
+    UNSTRETCH_FACTOR = opts.unstretchFactor ?? UNSTRETCH_FACTOR;
+    MAX_CHARS =
+      (opts.maximumCharacters === -1 ? Infinity : opts.maximumCharacters) ??
+      MAX_CHARS;
+    PATH_COUNT = opts.numberOfHeads ?? PATH_COUNT;
+  } catch (e) {
+    console.error("Invalid configuration file! Error:", e);
+    process.exit(1);
+  }
+} else {
+  console.error(`Warning: ${cfgPath} not found, resorting to defaults`);
+  delay = true;
+}
 
 function cursorTo(x, y) {
   process.stdout.write(`\u001b[${y + 1};${x + 1}f`);
@@ -63,8 +147,10 @@ function getNextDir(step, dir) {
   let randFact = step - MIN_DIST_TURN;
   if (randFact < 0) return dir;
   if (RAND_MODE === 0) randFact = 1.0 - (1.0 - RAND_AMOUNT) ** step;
-  else randFact = 1.0 - RAND_AMOUNT;
-  randFact *= UNSTRETCH_VERTICAL && dir % 2 === 0 ? UNSTRETCH_FACTOR : 1;
+  // Exponentially increase chance as time goes on
+  else randFact = 1.0 - RAND_AMOUNT; // Just based on the value
+  randFact *= UNSTRETCH_VERTICAL && dir % 2 === 0 ? UNSTRETCH_FACTOR : 1; // Unstretch literally just increases the chance
+  // for paths going vertically to switch to horizontal
   if (Math.random() > randFact) return dir;
   let nd = Math.random() < 0.5 ? dir - 1 : dir + 1;
   if (nd < 0) nd = 3;
@@ -79,6 +165,7 @@ function getRandColor() {
 
 function applyDir(pos, dir) {
   let np;
+  // Y is inverse
   switch (dir) {
     case 0:
       np = [pos[0], pos[1] - 1]; // Up
@@ -106,9 +193,9 @@ let charCount = 0;
 function tick(cDir, cPos, cStep, color) {
   return new Promise((res) => {
     if (charCount >= MAX_CHARS)
-      process.stdout.write("\x1b[2J"), (charCount = 0);
+      process.stdout.write("\x1b[2J"), (charCount = 0); // Clear screen and reset character count
     let nd = getNextDir(cStep, cDir);
-    process.stdout.write(`\x1b[1;38;5;${color}m`);
+    process.stdout.write(`\x1b[1;38;5;${color}m`); // Set color
     writeChar(cDir, nd, cPos);
     let pPos = cPos;
     cPos = applyDir(cPos, nd);
@@ -122,21 +209,27 @@ function tick(cDir, cPos, cStep, color) {
   });
 }
 
-process.stdout.write("\x1b[2J\x1b[?25l");
+function start() {
+  process.stdout.write("\x1b[2J\x1b[?25l"); // Clear screen and hide cursor
+  // tick() returns a promise, so we can just loop the number of paths we want and call them and they will all run asynchronously
+  // in parallel
+  for (let i = 0; i < PATH_COUNT; i++) {
+    tick(
+      Math.floor(Math.random() * 4),
+      [
+        Math.floor(process.stdout.columns / 2),
+        Math.floor(process.stdout.rows / 2),
+      ],
+      0,
+      getRandColor()
+    );
+  }
 
-for (let i = 0; i < PATH_COUNT; i++) {
-  tick(
-    Math.floor(Math.random() * 4),
-    [
-      Math.floor(process.stdout.columns / 2),
-      Math.floor(process.stdout.rows / 2),
-    ],
-    0,
-    getRandColor()
-  );
+  process.stdin.setRawMode(true); // Raw mode is set so that any keypress will close the program, even without a newline
+  process.stdin.on("data", () => process.exit(1));
+
+  process.on("exit", () => process.stdout.write("\x1b[?25h")); // Show cursor when the process exits
 }
 
-process.stdin.setRawMode(true);
-process.stdin.on("data", () => process.exit(1));
-
-process.on("exit", () => process.stdout.write("\x1b[?25h"));
+if (delay) setTimeout(start, 500);
+else start();
